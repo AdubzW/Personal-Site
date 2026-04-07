7863b34dd7d2c92d71853922f907c226019a2da1
AlexW <alexanderweitzel@outlook.com>
2026-04-06 17:13:07 -0700

    Rewrite
    Rewrite layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -62,17 +62,7 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(
-      filterFn: (node) => {
-    // set containing names of everything you want to filter out
-    const omit = new Set(["Events","Recipes"])
- 
-    // can also use node.slug or by anything on node.data
-    // note that node.data is only present for files that exist on disk
-    // (e.g. implicit folder nodes that have no associated index.md)
-    return !omit.has(node.displayName.toLowerCase())
-  },
-    ),
+    Component.Explorer(),
   ],
   right: [],
 }
